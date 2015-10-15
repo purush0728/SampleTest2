dbd87f95e7443d465e9e201e939b79d416e7b5e7
laxman <laxman@rizecorp.com>
2015-10-15 16:51:53 +0530

    updatedgl;

--- SampleGit/ViewController.swift
+++ SampleGit/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        println("My First Git hub demo")
+        println("My Second git hub demo")
     }
 
     override func didReceiveMemoryWarning() {
